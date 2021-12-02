78c82e218153e1c13397cd27677f3e67968dfa7a
candacerubin <candacerubin0@gmail.com>
2021-12-02 00:46:20 -0800

    comments; server configs

--- server/app.ts
+++ server/app.ts
@@ -1,14 +1,23 @@
 import express, { Application, Request, Response } from 'express';
+import * as dotenv from 'dotenv';
 
+// initialize Express inside Typescript
 const app: Application = express();
+
+// config setup
+dotenv.config({ path: 'server/config/config.env' });
+
+// set server port
 const PORT = process.env.PORT || 8000;
 
+// set endpoints/routes
 app.get('/', (req: Request, res: Response): void => {
-	res.send('hey yall');
+	res.send('The server is functioning properly');
 });
 
+// listen to server port
 app.listen(PORT, (): void => {
-	console.log(`connected on port ${PORT}`);
+	console.log(
+		`Server running at http://localhost:${PORT} in ${process.env.NODE_ENV} mode`
+	);
 });
-
-export default app;
